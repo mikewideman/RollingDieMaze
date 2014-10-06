fef8d02c3cd85725a13f1fecaac799d440beef97
Mike Wideman <wideman.mb@gmail.com>
2014-10-06 18:36:15 -0400

    Adding MazeState.py

---
+++ MazeState.py
@@ -0,0 +1,10 @@
+# MazeState.py
+# Developed for CSCI-331 at RIT
+# Authors: Mike Wideman, Dan Lavoie
+# Description: Provides a class representing the state of a Rolling Die Maze
+
+class MazeState:
+    maze = []
+
+    def __init__(filename):
+        return
