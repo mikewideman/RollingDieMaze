9bc8d740ebc98a8ed7f7a872861de97f72ebb356
Mike Wideman <wideman.mb@gmail.com>
2014-10-06 20:16:51 -0400

    Defined getChildStates and fixed various things

--- MazeState.py
+++ MazeState.py
@@ -1,9 +1,11 @@
 # MazeState.py
 # Developed for CSCI-331 at RIT
 # Authors: Mike Wideman, Dan Lavoie
-# Description: Provides a class representing the state of a Rolling Die Maze
+# Description: Provides a class representing the state of a Rolling Die Maze.
+# Also provides a Die class representing the state of a standard six-sided die.
 
 from enum import Enum
+import copy
 class Moves(Enum):
     north=1
     east=2
@@ -18,8 +20,8 @@ class Spaces:
 class Die:
     def __init__(self):
         self.activeFace = 1
-        self.westSide = 4
-        self.northSide = 2
+        self.westFace = 4
+        self.northFace = 2
     
     def rotate(self, direction):
         startActive = self.activeFace
@@ -36,9 +38,9 @@ class Die:
             self.westFace = startActive
         if direction == Moves.south:
             self.activeFace = startNorth
-            self.startNorth = 7-active
+            self.startNorth = 7-startActive
 
-    def getCurrentFace(self):
+    def getActiveFace(self):
         return self.activeFace
         
 class MazeState:
@@ -49,7 +51,7 @@ class MazeState:
         self.goal = ()
         self.readFile(file)
         self.die = Die()
-        self.current = ()
+        self.current = (0,0)
         
     def readFile(self, filename):
         file = open(filename)
@@ -71,12 +73,35 @@ class MazeState:
                     self.goal = (curLine,curChar)
                 curChar+=1
             curLine+=1
-            
+
+    def isValidSpace(self,x,y):
+        return x >= 0 and y >=0 and x < len(self.maze) and y < len(self.maze[x]) and not self.maze[x][y] == '*'
+
     def getChildStates(self):
-        return []
+        children = []
+        possibleMoves = [Moves.north, Moves.east, Moves.south, Moves.west]
+        for move in possibleMoves:
+            childCandidate = copy.deepcopy(self)
+            nextSpace = self.current
+            if move == Moves.north:
+                nextSpace = (self.current[0]-1, self.current[1])
+            elif move == Moves.south:
+                nextSpace = (self.current[0]+1, self.current[1])
+            elif move == Moves.east:
+                nextSpace = (self.current[0], self.current[1]+1)
+            elif move == Moves.west:
+                nextSpace = (self.current[0], self.current[1]-1)
+            if self.isValidSpace(nextSpace[0], nextSpace[1]):
+                childCandidate.maze[self.current[0]][self.current[1]] = Spaces.free
+                childCandidate.maze[nextSpace[0]][nextSpace[1]] = Spaces.current
+                childCandidate.current = nextSpace
+                childCandidate.die.rotate(move)
+                if not childCandidate.die.getActiveFace() == 6:
+                    children.append(childCandidate)
+        return children
 
     def isSolution(self):
-        return current == goal
+        return self.current == self.goal
     
     def printMaze(self):
         curLine = 0
@@ -84,7 +109,7 @@ class MazeState:
         for line in self.maze:
             for space in line:
                 if space == Spaces.current:
-                    print(str(self.die.getCurrentFace()), end="")
+                    print(str(self.die.getActiveFace()), end="")
                 elif space == Spaces.obstacle:
                     print('*', end="")
                 elif space == Spaces.free:
@@ -98,6 +123,12 @@ class MazeState:
             curLine+=1
             print() #print newline
 
-
-maze = MazeState("maze.dat.txt")
-maze.printMaze()
+#Test code for this module
+#maze = MazeState("maze.dat.txt")
+#children = maze.getChildStates()
+#nextChildren = children[0].getChildStates()
+#maze.printMaze()
+#for child in children:
+#    child.printMaze()
+#for child in nextChildren:
+#    child.printMaze()
